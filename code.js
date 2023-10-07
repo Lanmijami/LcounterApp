b7cfd792ca02aeed588c9eac45099392446fa911
Lanmijami <milanvukasinovic5@gmail.com>
2023-10-07 21:40:04 +0200

    Dodao reset dugme

--- code.js
+++ code.js
@@ -1,7 +1,14 @@
 let broj = 0;
+localStorage.setItem("myKey", "testValue");
+
 function Klik() {
   broj++;
   tekstPolje = document.getElementById("tekst");
-  console.log("Radi");
   tekstPolje.innerHTML = broj;
+}
+
+function Resetuj() {
+  tekstPolje = document.getElementById("tekst");
+  tekstPolje.innerHTML = 0;
+  broj = 0;
 }
